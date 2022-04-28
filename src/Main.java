60250a40871b675447b39eaa75d8afd6f50a5d7c
krcut <104581585+krcut@users.noreply.github.com>
2022-04-29 00:57:08 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String args[]){
         System.out.println("test page");
         System.out.println("test dongmin");
+        System.out.println("test2");
     }
 }
